51611327e69dec32c10205ea9763f80b82dcf476
feredico.gattuso <feredico.gattuso@gmail.com>
2022-10-07 13:21:41 +0300

    Добавьте файлы проекта.

---
+++ LaboratoryThreads/NumericMatrix.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+
+namespace LaboratoryThreads
+{
+    internal sealed class NumericMatrix
+    {
+        private int rows;
+        private int columns;
+        private double[,] matrix;
+
+        public int Rows
+        {
+            get => rows;
+            private set => rows = value;
+        }
+        public int Columns
+        {
+            get => columns;
+            private set => columns = value;
+        }
+        public double this[int rowIndex, int columnIndex] 
+        {
+            get => matrix[rowIndex, columnIndex];
+            set => matrix[rowIndex, columnIndex] = value;
+        }
+
+
+        public NumericMatrix(int rows, int columns)
+        {
+            (Rows, Columns) = (rows, columns);
+            matrix = new double[Rows, Columns];
+        }
+        public NumericMatrix(double[,] array)
+        {
+            Rows = array.GetLength(0);
+            Columns = array.GetLength(1);
+
+            matrix = (double[,])array.Clone();
+        }
+        private NumericMatrix Multiply(NumericMatrix other)
+        {
+            if (!Columns.Equals(other.Rows))
+            {
+                throw new ArgumentException("Current column count does not equal other matrix row count.");
+            }
+            else
+            {
+                var resultMatrix = new NumericMatrix(Rows, other.Columns);
+
+                for (int i = 0; i < Rows; i++)
+                {
+                    for (int j = 0; j < other.Columns; j++)
+                    {
+                        for (int k = 0; k < Columns; k++)
+                        {
+                            resultMatrix[i, j] += matrix[i, k] * other[k, j];
+                        }
+                    }
+                }
+                return resultMatrix;
+            }   
+        }
+        public static NumericMatrix operator *(NumericMatrix first, NumericMatrix second) => first.Multiply(second);
+        public sealed override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    stringBuilder.Append(matrix[i, j] + "\t");
+                }
+                stringBuilder.Append("\n");
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ LaboratoryThreads/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LaboratoryThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var firstArray = new double[,]
+            {
+                {1,2,3,0 },
+                {4,6,7,2 },
+                {1,2,3,0 }
+            };
+
+            var secondArray = new double[,]
+            {
+                {3,2 },
+                {4,2 },
+                {7,2 },
+                {1,2 }
+            };
+
+            var watch = new Stopwatch();
+
+            NumericMatrix first = new NumericMatrix(firstArray);
+            NumericMatrix second = new NumericMatrix(secondArray);
+
+            watch.Start();
+            var result = first * second;
+            watch.Stop();
+
+            Console.WriteLine("Result:");
+            Console.WriteLine(result.ToString());
+            Console.WriteLine("Spent time in ms: " + watch.Elapsed.TotalMilliseconds);
+
+        
+
+
+          
+
+      
+
+
+
+
+        }
+    }
+}
